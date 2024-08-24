71a5b0748b955cb568ae451e26a91a4f8d18d453
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-08-24 22:48:20 +0530

    Update typewise-alert.tests.cpp - Added TO_EMAIL test cases

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -3,10 +3,6 @@
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
 
-  EXPECT_EQ(inferBreach(-2.5, 0, 35), TOO_LOW);
-  EXPECT_EQ(inferBreach(-2.5, 0, 40), TOO_LOW);
-  EXPECT_EQ(inferBreach(-2.5, 0, 45), TOO_LOW);
-
   BatteryCharacter batteryA = {PASSIVE_COOLING, "Brand_A"};
   BatteryCharacter batteryB = {MED_ACTIVE_COOLING, "Brand_B"};
   BatteryCharacter batteryC = {HI_ACTIVE_COOLING, "Brand_C"};
@@ -22,4 +18,16 @@ TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
   checkAndAlert(TO_CONTROLLER, batteryC, -10);
   checkAndAlert(TO_CONTROLLER, batteryC, 42);
   checkAndAlert(TO_CONTROLLER, batteryC, 46);
+
+  checkAndAlert(TO_EMAIL, batteryA, -2.5);
+  checkAndAlert(TO_EMAIL, batteryA, 5);
+  checkAndAlert(TO_EMAIL, batteryA, 36);
+
+  checkAndAlert(TO_EMAIL, batteryB, -1);
+  checkAndAlert(TO_EMAIL, batteryB, 36);
+  checkAndAlert(TO_EMAIL, batteryB, 41);
+
+  checkAndAlert(TO_EMAIL, batteryC, -10);
+  checkAndAlert(TO_EMAIL, batteryC, 42);
+  checkAndAlert(TO_EMAIL, batteryC, 46);
 }
