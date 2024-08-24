161b6df8379b775edc8fa4bdac368bae1df71349
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-08-24 20:28:28 +0530

    Update typewise-alert.h - cooling limits as macros

--- typewise-alert.h
+++ typewise-alert.h
@@ -30,3 +30,10 @@ void checkAndAlert(
 
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
+
+#define COOLING_LOW_LIM 0
+#define PASSIVE_COOL_HI_LIM 35
+
+#define MEDIUM_ACTIVE_COOL_HI_LIM 40
+
+#define HI_ACTIVE_COOL_HI_LIM 45
