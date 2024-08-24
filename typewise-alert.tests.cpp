d024b659c10a8ad6849aeaabf3ac398b2bee4d99
Sreejith S <51960515+Sreejith-Sasikumar@users.noreply.github.com>
2024-08-24 23:08:27 +0530

    Update typewise-alert.tests.cpp - test cases' comments

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -7,27 +7,24 @@ TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
   BatteryCharacter batteryB = {MED_ACTIVE_COOLING, "Brand_B"};
   BatteryCharacter batteryC = {HI_ACTIVE_COOLING, "Brand_C"};
 
-  checkAndAlert(TO_CONTROLLER, batteryA, -2.5);
-  checkAndAlert(TO_CONTROLLER, batteryA, 5);
-  checkAndAlert(TO_CONTROLLER, batteryA, 36);
+  checkAndAlert(TO_CONTROLLER, batteryA, -2.5);   //Tests PASSIVE_COOLING TOO_LOW breach alert TO_CONTROLLER
+  checkAndAlert(TO_CONTROLLER, batteryA, 5);      //Tests PASSIVE_COOLING NORMAL message TO_CONTROLLER
+  checkAndAlert(TO_CONTROLLER, batteryA, 36);     //Tests PASSIVE_COOLING TOO_HIGH breach alert TO_CONTROLLER
 
-  checkAndAlert(TO_CONTROLLER, batteryB, -1);
-  checkAndAlert(TO_CONTROLLER, batteryB, 36);
-  checkAndAlert(TO_CONTROLLER, batteryB, 41);
+  checkAndAlert(TO_CONTROLLER, batteryB, -1);    //Tests MED_ACTIVE_COOLING TOO_LOW breach alert TO_CONTROLLER
+  checkAndAlert(TO_CONTROLLER, batteryB, 36);    //Tests MED_ACTIVE_COOLING NORMAL message TO_CONTROLLER
+  checkAndAlert(TO_CONTROLLER, batteryB, 41);    //Tests MED_ACTIVE_COOLING TOO_HIGH breach alert TO_CONTROLLER
 
-  checkAndAlert(TO_CONTROLLER, batteryC, -10);
-  checkAndAlert(TO_CONTROLLER, batteryC, 42);
-  checkAndAlert(TO_CONTROLLER, batteryC, 46);
+  checkAndAlert(TO_CONTROLLER, batteryC, -10);  //Tests HI_ACTIVE_COOLING TOO_LOW breach alert TO_CONTROLLER
+  checkAndAlert(TO_CONTROLLER, batteryC, 42);   //Tests HI_ACTIVE_COOLING NORMAL message TO_CONTROLLER
+  checkAndAlert(TO_CONTROLLER, batteryC, 46);   //Tests HI_ACTIVE_COOLING TOO_HIGH breach alert TO_CONTROLLER
 
-  checkAndAlert(TO_EMAIL, batteryA, -2.5);
-  checkAndAlert(TO_EMAIL, batteryA, 5);
-  checkAndAlert(TO_EMAIL, batteryA, 36);
+  checkAndAlert(TO_EMAIL, batteryA, -2.5);    //Tests PASSIVE_COOLING TOO_LOW breach alert TO_EMAIL
+  checkAndAlert(TO_EMAIL, batteryA, 36);      //Tests PASSIVE_COOLING TOO_HIGH breach alert TO_EMAIL
 
-  checkAndAlert(TO_EMAIL, batteryB, -1);
-  checkAndAlert(TO_EMAIL, batteryB, 36);
-  checkAndAlert(TO_EMAIL, batteryB, 41);
+  checkAndAlert(TO_EMAIL, batteryB, -1);    //Tests MED_ACTIVE_COOLING TOO_LOW breach alert TO_EMAIL
+  checkAndAlert(TO_EMAIL, batteryB, 41);    //Tests MED_ACTIVE_COOLING TOO_HIGH breach alert TO_EMAIL
 
-  checkAndAlert(TO_EMAIL, batteryC, -10);
-  checkAndAlert(TO_EMAIL, batteryC, 42);
-  checkAndAlert(TO_EMAIL, batteryC, 46);
+  checkAndAlert(TO_EMAIL, batteryC, -10);    //Tests HI_ACTIVE_COOLING TOO_LOW breach alert TO_EMAIL
+  checkAndAlert(TO_EMAIL, batteryC, 46);     //Tests HI_ACTIVE_COOLING TOO_HIGH breach alert TO_EMAIL
 }
